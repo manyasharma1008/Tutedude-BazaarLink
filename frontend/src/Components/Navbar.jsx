592d9cd23ce11865f9a9078fb06bc06691f0ffbc
MJ-1112 <m22327803@gmail.com>
2025-07-27 11:54:23 +0530

    Tailwind Css added

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Dvelopers</li>
+                <li>Contat Us</li>
+                <button>Login</button>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
